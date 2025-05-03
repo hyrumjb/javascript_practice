a5aa6850327e65ed6afe87aebd1bae1d4d483b44
Hyrum Bradshaw <bradshaw.hyrum@gmail.com>
2025-05-03 10:25:18 -0600

    add script file

---
+++ myScript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+// Technically with innerHTML you change the whole element, while innerText changes just the plain text'
+
+// document.write is only used for testing because it literally deletes the content
+
+// I've used window.alert() some for putting a notification on their screen (also, you can just say alert)
